c0428f880f3933abc7102c24d0449cc84665555a
Paul W. Egeler, M.S., GStat <paul.egeler@spectrumhealth.org>
2019-06-24 13:48:18 -0400

    moved metadata names into a sysdata file

--- R/R/REDCap_split.r
+++ R/R/REDCap_split.r
@@ -87,14 +87,7 @@ REDCap_split <- function(records, metadata) {
   }
 
   # Standardize variable names for metadata
-  names(metadata) <- c(
-    "field_name", "form_name", "section_header", "field_type",
-    "field_label", "select_choices_or_calculations", "field_note",
-    "text_validation_type_or_show_slider_number", "text_validation_min",
-    "text_validation_max", "identifier", "branching_logic", "required_field",
-    "custom_alignment", "question_number", "matrix_group_name", "matrix_ranking",
-    "field_annotation"
-  )
+  names(metadata) <- metadata_names
 
   # Make sure that no metadata columns are factors
   metadata <- rapply(metadata, as.character, classes = "factor", how = "replace")
