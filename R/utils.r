4cd484bd6bd1ca7c3836d31a06d6dfa5a3bf2188
Andreas Gammelgaard Damsbo <andreas@gdamsbo.dk>
2024-04-09 10:54:52 +0200

    new version

--- R/utils.r
+++ R/utils.r
@@ -506,6 +506,7 @@ is_repeated_longitudinal <- function(data, generics = c(
 #'
 #' @examples
 #' file_extension(list.files(here::here(""))[[2]])[[1]]
+#' file_extension(c("file.cd..ks","file"))
 file_extension <- function(filenames) {
   sub(pattern = "^(.*\\.|[^.]+)(?=[^.]*)", replacement = "",
       filenames,
@@ -533,8 +534,11 @@ read_input <- function(file, consider.na = c("NA", '""', "")) {
         df <- openxlsx2::read_xlsx(file = file, na.strings = consider.na)
       } else if (ext == "dta") {
         df <- haven::read_dta(file = file)
+      } else if (ext == "ods") {
+        df <- readODS::read_ods(file = file)
       } else {
-        stop("Input file format has to be either '.csv', '.xls' or '.xlsx'")
+        stop("Input file format has to be on of:
+             '.csv', '.xls', '.xlsx', '.dta' or '.ods'")
       }
     },
     error = function(e) {
